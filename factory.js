f913c07f3a70153929742226f594b8a1d84f83c4
MaryImarenezor <141180510+MaryImarenezor@users.noreply.github.com>
2023-09-10 21:44:09 +0000

    completed assignment

--- factory.js
+++ factory.js
@@ -1,3 +1,25 @@
 class Factory{
-    
+    constructor(location, owner, vehicle, vin, paintColor) {
+        this.location = location;
+        this.owner = owner;
+        this.vehicle = undefined;
+        this.vin = undefined;
+        this.paintColor = undefined;
+
+    }
+    createVehicle(vehicle) {
+        this.vehicle = vehicle
+    }
+    listVehicles() {
+        return this.vehicle
+    }
+    paintVehicle(vin, paintColor) {
+        this.vin = vin;
+        this.paintColor = paintColor;
+    }
 }
+
+let factory = new Factory("Toyota", "Andrew Burr", undefined, undefined, undefined)
+factory.createVehicle("Toyota Highlander")
+factory.listVehicles()
+factory.paintVehicle("1234", "white")
--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,20 @@
 class Vehicle {
-    
+    constructor(make, model, color, price, vin, year) {
+        this.make = make;
+        this.model = model;
+        this.color = undefined;
+        this.price = price;
+        this.vin = vin;
+        this.year = year;
+    }
+    drive() {
+        this.year + " " + this.make + " " + this.model + " " + "drives"
+    }
+    paint(color) {
+        this.color = color
+    }
 }
 
+let ford = new Vehicle("Ford", "F-150", undefined, 19000, 1234, 2015)
+Vehicle.drive()
+Vehicle.paint("blue")
