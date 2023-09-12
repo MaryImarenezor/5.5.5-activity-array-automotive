1d0ff3f2d8e0443b0f0e432d3aebb0485e2062f8
MaryImarenezor <141180510+MaryImarenezor@users.noreply.github.com>
2023-09-12 21:52:38 +0000

    updated vehicle.js

--- vehicle.js
+++ vehicle.js
@@ -1,21 +1,19 @@
 class Vehicle {
-    constructor(make, model, price, vin, year) {
-        this.make = make;
-        this.model = model;
-        this.price = price;
-        this.vin = vin;
-        this.year = year;
+    constructor(make, model, year, price, vin){
+        this.make = make
+        this.model = model
+        this.year = year
+        this.price = price
+        this.vin = vin
+        this.color = undefined
     }
-    drive() {
-        this.year + " " + this.make + " " + this.model + " " + "drives"
-    }
-    paint(color) {
+    //paint method
+    paint(color){
         this.color = color
     }
-}
 
-let ford = new Vehicle("Ford", "F-150", undefined, 19000, 1234, 2015)
-Vehicle.drive()
-Vehicle.paint("blue")
-//look over panompto video for help
-//how to open html sites: https://stackoverflow.com/questions/74452866/how-preview-a-html-file-github-codespaces
+    drive(){
+        return this.year + " " + this.make + " " + this.model+ " drives"
+    }
+
+}
